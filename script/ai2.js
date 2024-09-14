e8ca6ce9a6c995e0b289834a75cbff6491d0ebf6
Puyit321 <jkingpuyit@gmail.com>
2024-09-14 21:51:22 +0800

    Update ai2.js

--- script/ai2.js
+++ script/ai2.js
@@ -43,7 +43,7 @@ module.exports.run = async function({ api, event, args }) {
       ...url
     });
 
-    api.sendMessage(`${data.message}`, event.threadID, event.messageID);
+    api.sendMessage(`•| 𝙱𝙾𝙶𝙰𝚁𝚃 𝙰𝙸 𝙱𝙾𝚃 |•\n\n${data.message}`, event.threadID, event.messageID);
     
   } catch {
     api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);

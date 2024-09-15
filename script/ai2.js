975d6e8dd394e6862d3344fbbff909591cb7bab5
Puyit321 <jkingpuyit@gmail.com>
2024-09-15 17:42:05 +0800

    Update ai2.js

--- script/ai2.js
+++ script/ai2.js
@@ -28,7 +28,7 @@ module.exports.run = async function ({ api, event, args }) {
     try {
         await api.sendMessage('☄️ 𝑮𝑬𝑴𝑰𝑵𝑰\n━━━━━━━━━━━━━━━━━━\nGemini recognizing picture, please wait...\n━━━━━━━━━━━━━━━━━━', event.threadID);
 
-        const response = await axios.get(`https://joshweb.click/gemini?prompt=${encodeURIComponent(prompt)}&url=${url}`);
+        const response = await axios.get(`https://deku-rest-api.gleeze.com/gemini?prompt=${encodeURIComponent(prompt)}&url=${url}`);
         const description = response.data.gemini;
 
         return api.sendMessage(`☄️ 𝑮𝑬𝑴𝑰𝑵𝑰\n━━━━━━━━━━━━━━━━━━\n${description}\n━━━━━━━━━━━━━━━━━━`, event.threadID, event.messageID);

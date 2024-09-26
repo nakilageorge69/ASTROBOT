804012ef3499cd144bf10d68e53f3672e1f7b0f7
geothe143 <geothe135@gmail.com>
2024-09-27 01:03:40 +0800

    Update ai2.js

--- script/ai2.js
+++ script/ai2.js
@@ -27,7 +27,7 @@ module.exports.run = async function ({ api, event, args }) {
         const response = await axios.get(`https://deku-rest-api.gleeze.com/gemini?prompt=${encodeURIComponent(prompt)}&url=${url}`);
         const description = response.data.gemini;
 
-        return api.sendMessage(`🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀\n━━━━━━━━━━━━━━━━━\n${description}\n━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕\n\nUse 👉ai👈 to answer only on text questions.`, event.threadID, event.messageID);
+        return api.sendMessage(`✧✧🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀✧✧\n━━━━━━━━━━━━━━━━━\n${description}\n━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕\n\nUse 👉ai👈 to answer only on text questions.`, event.threadID, event.messageID);
     } catch (error) {
         console.error(error);
         return api.sendMessage('✧✧🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀✧✧\n━━━━━━━━━━━━━━━━━\nTsskk...🙄 ayusin monga tanong mo 🤨.\n━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕', event.threadID, event.messageID);

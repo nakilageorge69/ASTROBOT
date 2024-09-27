70c201aed3faf51a56149d70ef362cec8c33ccbc
geothe143 <geothe135@gmail.com>
2024-09-27 17:57:40 +0800

    Update welcomenoti.js

--- script/event/welcomenoti.js
+++ script/event/welcomenoti.js
@@ -34,7 +34,7 @@ module.exports.handleEvent = async function ({ api, event }) {
             fs.writeFileSync(filePath, Buffer.from(data));
 
             api.sendMessage({
-                body: `🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀\n━━━━━━━━━━━━━━━━━━\n💕 Everyone Let's Welcome Our New Member${name} to ${groupName}! 💕\n━━━━━━━━━━━━━━━━━━\n💕 ғʀᴏᴍ: ᴀᴅᴍɪɴ ɢᴇᴏʀᴀʏ 💕`,
+                body: `🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀\n━━━━━━━━━━━━━━━━━━\n💕 Everyone Let's Welcome Our New Member${name} to ${groupName}! 💕\n━━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕`,
                 attachment: fs.createReadStream(filePath)
             }, event.threadID, () => fs.unlinkSync(filePath));
         } catch (error) {
@@ -42,7 +42,7 @@ module.exports.handleEvent = async function ({ api, event }) {
 
             // Fallback message if fetching the image fails
             api.sendMessage({
-                body: `✧✧🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀✧✧\n━━━━━━━━━━━━━━━━━━\n Everyone Let's Welcome Our New Member 👉 ${name} 👈 to ${groupName}!\n━━━━━━━━━━━━━━━━━━\n💕 ғʀᴏᴍ: ᴀᴅᴍɪɴ ɢᴇᴏʀᴀʏ 💕`
+                body: `✧✧🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀✧✧\n━━━━━━━━━━━━━━━━━━\n Everyone Let's Welcome Our New Member 👉 ${name} 👈 to ${groupName}!\n━━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕`
             }, event.threadID);
         }
     }

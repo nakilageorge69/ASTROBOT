4c0527fb25a257d81bfca8c193a753cca283363a
geothe143 <geothe135@gmail.com>
2024-09-27 17:23:50 +0800

    Create inform.js

---
+++ script/inform.js
@@ -0,0 +1,41 @@
+module.exports.config = {
+    name: "inform",
+    version: "1.0.0",
+    hasPermssion: 2,
+    credits: "GeoDevz69",
+    description: "Sends a message to all groups and can only be done by the admin.",
+    usePrefix: true,
+    commandCategory: "noti",
+    usages: "[Text]",
+    cooldowns: 5
+};
+
+module.exports.run = async ({ api, event, args }) => {
+    const threadList = await api.getThreadList(25, null, ['INBOX']);
+    let sentCount = 0;
+    const custom = args.join(' ');
+
+    async function sendMessage(thread) {
+        try {
+            await api.sendMessage(`𝙉𝙊𝙏𝙄𝘾𝙀 𝙁𝙍𝙊𝙈 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍\n ----------------\n Developer Name: gdevz69\n ---------------\n\n『𝗡𝗼𝘁𝗶𝗰𝗲』"${custom}"`, thread.threadID);
+            sentCount++;
+        } catch (error) {
+            console.error("Error sending a message:", error);
+        }
+    }
+
+    for (const thread of threadList) {
+        if (sentCount >= 20) {
+            break;
+        }
+        if (thread.isGroup && thread.name != thread.threadID && thread.threadID != event.threadID) {
+            await sendMessage(thread);
+        }
+    }
+
+    if (sentCount > 0) {
+        api.sendMessage(`› Sent the notification successfully.`, event.threadID);
+    } else {
+        api.sendMessage("› No eligible group threads found to send the message to.", event.threadID);
+    }
+};

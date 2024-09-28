7fa7333e7279a31d140a806a61715e21102f8b0d
geothe143 <geothe135@gmail.com>
2024-09-28 18:55:05 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -25,13 +25,13 @@ module.exports.run = async function({
       let page = 1;
       let start = (page - 1) * pages;
       let end = start + pages;
-      let helpMessage = `✧✧🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀✧✧\n━━━━━━━━━━━━━━━━━\n🔰 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧:\n\n`;
+      let helpMessage = `╭─『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 𝗟𝗜𝗦𝗧 』\n`;
       for (let i = start; i < Math.min(end, commands.length); i++) {
-        helpMessage += `✧\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
+        helpMessage += `|✧「 ${prefix}${commands[i]} 」\n╰────────────────◊`;
       }
-      helpMessage += '\n🔰 𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦𝗧:\n\n';
+      helpMessage += '╭─『 𝗘𝗩𝗘𝗡𝗧 𝗟𝗜𝗦𝗧 』\n';
       eventCommands.forEach((eventCommand, index) => {
-        helpMessage += `✧\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
+        helpMessage += `|✧「 ${prefix}${eventCommand} 」\n╰────────────────◊\n;
       });
       helpMessage += `\nPage ${page}/${Math.ceil(commands.length / pages)}. To view the next page, type '${prefix}help page number'. To view information about a specific command, type '${prefix}help command name'.\n━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕`;
       api.sendMessage(helpMessage, event.threadID, event.messageID);

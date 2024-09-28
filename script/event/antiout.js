cab88da2905e3f7d82153d99ab8641e3ea630153
geothe143 <geothe135@gmail.com>
2024-09-28 22:20:17 +0800

    Update antiout.js

--- script/event/antiout.js
+++ script/event/antiout.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = async ({
       if (error) {
         api.sendMessage(`Unable to re-add member ${name} to the group!`, event.threadID);
       } else {
-        api.sendMessage(`✧✧🎀 𝗚𝗖𝗛𝗔𝗧 𝗕𝗢𝗧 🎀✧✧\n━━━━━━━━━━━━━━━━━━\nOppsss..! ${name} wag kang umalis mamiss ka namin.\n━━━━━━━━━━━━━━━━━━\n💕 ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ 💕`, event.threadID);
+        api.sendMessage(`╭─『 𝗡𝗢 𝗦𝗖𝗔𝗣𝗘 』✧✧✧\n╰✧✧✧───────────✧\nOppsss..! ${name} wag kang umalis mamiss ka namin.\n╭✧✧✧───────────✧\n   ᴏᴡɴᴇʀ : ɢᴇᴏʀɢᴇ ɴᴀᴋɪʟᴀ\n╰─────────────✧✧✧`, event.threadID);
       }
     });
   }

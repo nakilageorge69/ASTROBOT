0041866cf61773311fa3b0c3e0d6d480643fec70
geothe143 <geothe135@gmail.com>
2024-10-05 21:29:37 +0800

    Create leave.js

---
+++ script/leave.js
@@ -0,0 +1,20 @@
+module.exports.config = {
+  name: "leave",
+  version: "1.0.0",
+  role: 2,
+  hasPrefix: true,
+  credits: "GeoDevz69",
+  description: "Bot leaves the thread",
+  usages: "leave",
+  cooldowns: 5,
+
+};
+
+module.exports.run = async function({ api, event, args }) {
+  try { 
+  if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+  if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+    } catch (error) {
+      api.sendMessage(error.message, event.threadID, event.messageID);
+    }
+};

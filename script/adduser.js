76f7c1e86bb7be163789e6447e6fa0b483e43ff3
geothe143 <geothe135@gmail.com>
2024-10-31 13:03:41 +0800

    Create adduser.js

---
+++ script/adduser.js
@@ -0,0 +1,33 @@
+module.exports.config = {
+    name: "adduser",
+    version: "1.0.0",
+    role: 0,
+    credits: "GeoDevz69",
+    description: "Add a user ",
+    hasPrefix: false,
+    aliases: ["add"],
+    usage: "[adduser <uid>]",
+    cooldown: 5
+};
+
+module.exports.run = async function({ api, event, args }) {
+    try {
+        const uid = args[0];
+        if (!uid) {
+            api.sendMessage("Usage: adduser <uid>", event.threadID);
+            return;
+        }
+
+        api.addUserToGroup(uid, event.threadID, (err) => {
+            if (err) {
+                api.sendMessage(`╭─『 𝗔𝗗𝗠𝗜𝗡𝗙𝗢 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: Failed to add user with UID ${uid} to the group.\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 𝚄𝙽𝚂𝙴𝙽𝙳 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧`, event.threadID);
+                console.error('Error:', err);
+            } else {
+                api.sendMessage(`╭─『 𝗔𝗗𝗠𝗜𝗡𝗙𝗢 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: Successfully added user with UID ${uid} to the group.\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 𝚄𝙽𝚂𝙴𝙽𝙳 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧`, event.threadID);
+            }
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        api.sendMessage("An error occurred while processing the request.", event.threadID);
+    }
+};

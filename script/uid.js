16f88183e7b10e4af5fab7a991c1f29ad9068306
geothe143 <geothe135@gmail.com>
2024-10-31 13:14:31 +0800

    Update uid.js

--- script/uid.js
+++ script/uid.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "uid",
 	role: 0,
-	credits: "Mirai Team",
+	credits: "GeoDevz69",
 	description: "Get the user's Facebook UID.",
 	hasPrefix: false,
 	usages: "{p}uid {p}uid @mention",
@@ -15,12 +15,12 @@ module.exports.run = async function({ api, event }) {
 			const senderID = event.messageReply.senderID;
 			return api.sendMessage(senderID, event.threadID);
 		} else {
-			return api.sendMessage(`${event.senderID}`, event.threadID, event.messageID);
+			return api.sendMessage(`╭─『 𝗬𝗢𝗨𝗥 𝗜𝗗 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: ${event.senderID}\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 𝚄𝙽𝚂𝙴𝙽𝙳 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧`, event.threadID, event.messageID);
 		}
 	} else {
 		for (const mentionID in event.mentions) {
 			const mentionName = event.mentions[mentionID];
-			api.sendMessage(`${mentionName.replace('@', '')}: ${mentionID}`, event.threadID);
+			api.sendMessage(`╭─『 𝗬𝗢𝗨𝗥 𝗜𝗗 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: ${mentionName.replace('@', '')}: ${mentionID}\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 𝚄𝙽𝚂𝙴𝙽𝙳 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧`, event.threadID);
 		}
 	}
 };

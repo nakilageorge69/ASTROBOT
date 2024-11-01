13a8c119d18055df92832064347e87b74cb59dcc
geothe143 <geothe135@gmail.com>
2024-11-01 20:34:16 +0800

    Update help.js

--- script/help.js
+++ script/help.js
@@ -7,6 +7,15 @@ module.exports["config"] = {
   role: 0,
   hasPrefix: true,
   aliases: ['info'],
+  description: "Beginner's guconst { OnChat, font } = require("chatbox-utility");
+
+
+module.exports["config"] = {
+  name: 'help',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: true,
+  aliases: ['info'],
   description: "Beginner's guide",
   usage: "Help [page] or [command]",
   credits: 'Cid',
@@ -97,7 +106,102 @@ module.exports["handleEvent"] = async function({
     messageID,
     body
   } = event;
-  const message = prefix ? 'This is my prefix: ' + prefix : "╭─『 𝗣𝗥𝗘𝗙𝗜𝗫 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: Sorry i don't have prefix, use my command directly without prefix.\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 '𝚄𝙽𝚂𝙴𝙽𝙳' 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸'𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧";
+  const message = prefix ? '╭─『 𝗣𝗥𝗘𝗙𝗜𝗫 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: This is my prefix: ' + prefix : \n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 '𝚄𝙽𝚂𝙴𝙽𝙳' 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸'𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧"╭─『 𝗣𝗥𝗘𝗙𝗜𝗫 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: Sorry i don't have prefix, use my command directly without prefix.\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 '𝚄𝙽𝚂𝙴𝙽𝙳' 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸'𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧";
+  if (body?.toLowerCase().startsWith('prefix')) {
+    api.sendMessage(message, threadID, messageID);
+  }
+}
+ide",
+  usage: "Help [page] or [command]",
+  credits: 'Cid',
+};
+module.exports["run"] = async function({
+  api,
+  event,
+  enableCommands,
+  args,
+  Utils,
+  prefix
+}) {
+  const chat = new OnChat(api, event);
+  const input = args.join(' ');
+  try {
+    const eventCommands = enableCommands[1].handleEvent;
+    const commands = enableCommands[0].commands;
+    if (!input) {
+      const pages = 20;
+      let page = 1;
+      let start = (page - 1) * pages;
+      let end = start + pages;
+      let helpMessage = `╭─『 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n`;
+      for (let i = start; i < Math.min(end, commands.length); i++) {
+        helpMessage += `│✧\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
+      }
+      helpMessage += '╰✧✧✧───────────✧\n╭─『 𝗘𝗩𝗘𝗡𝗧𝗦 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n';
+      eventCommands.forEach((eventCommand, index) => {
+        helpMessage += `│✧\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
+      });
+      helpMessage += `╰✧✧✧───────────✧\n◉ 𝚁𝙴𝙿𝙻𝚈 '𝚄𝙽𝚂𝙴𝙽𝙳' 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸'𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝙿𝙻𝙴𝙰𝚂𝙴 𝙳𝙾𝙽'𝚃 𝚂𝙿𝙰𝙼 𝚃𝙷𝙸𝚂 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧`;
+
+chat.reply({ body: helpMessage, attachment: await chat.stream("https://i.imgur.com/fhwp3lI.gif") });      
+      
+    } else if (!isNaN(input)) {
+      const page = parseInt(input);
+      const pages = 20;
+      let start = (page - 1) * pages;
+      let end = start + pages;
+      let helpMessage = `Command List:\n\n`;
+      for (let i = start; i < Math.min(end, commands.length); i++) {
+        helpMessage += `\t${i + 1}. 「 ${prefix}${commands[i]} 」\n`;
+      }
+      helpMessage += '\nEvent List:\n\n';
+      eventCommands.forEach((eventCommand, index) => {
+        helpMessage += `\t${index + 1}. 「 ${prefix}${eventCommand} 」\n`;
+      });
+      helpMessage += `\nPage ${page} of ${Math.ceil(commands.length / pages)}`;
+      api.sendMessage(helpMessage, event.threadID, event.messageID);
+    } else {
+      const command = [...Utils.handleEvent, ...Utils.commands].find(([key]) => key.includes(input?.toLowerCase()))?.[1];
+      if (command) {
+        const {
+          name,
+          version,
+          role,
+          aliases = [],
+          description,
+          usage,
+          credits,
+          cooldown,
+          hasPrefix
+        } = command;
+        const roleMessage = role !== undefined ? (role === 0 ? '➛ Permission: user' : (role === 1 ? '➛ Permission: admin' : (role === 2 ? '➛ Permission: thread Admin' : (role === 3 ? '➛ Permission: super Admin' : '')))) : '';
+        const aliasesMessage = aliases.length ? `➛ Aliases: ${aliases.join(', ')}\n` : '';
+        const descriptionMessage = description ? `Description: ${description}\n` : '';
+        const usageMessage = usage ? `➛ Usage: ${usage}\n` : '';
+        const creditsMessage = credits ? `➛ Credits: ${credits}\n` : '';
+        const versionMessage = version ? `➛ Version: ${version}\n` : '';
+        const cooldownMessage = cooldown ? `➛ Cooldown: ${cooldown} second(s)\n` : '';
+        const message = ` 「 Command 」\n\n➛ Name: ${name}\n${versionMessage}${roleMessage}\n${aliasesMessage}${descriptionMessage}${usageMessage}${creditsMessage}${cooldownMessage}`;
+        api.sendMessage(message, event.threadID, event.messageID);
+      } else {
+        api.sendMessage('Command not found.', event.threadID, event.messageID);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports["handleEvent"] = async function({
+  api,
+  event,
+  prefix
+}) {
+  const {
+    threadID,
+    messageID,
+    body
+  } = event;
+  const message = prefix ? '╭─『 𝗣𝗥𝗘𝗙𝗜𝗫 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: This is my prefix: ' + prefix : \n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 '𝚄𝙽𝚂𝙴𝙽𝙳' 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸'𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧"╭─『 𝗣𝗥𝗘𝗙𝗜𝗫 』✧✧✧\n╰✧✧✧───────────✧\n╭✧✧✧───────────✧\n𝙍𝙚𝙨𝙥𝙤𝙣𝙨𝙚: Sorry i don't have prefix, use my command directly without prefix.\n╰─────────────✧✧✧\n◉ 𝚁𝙴𝙿𝙻𝚈 '𝚄𝙽𝚂𝙴𝙽𝙳' 𝚃𝙾 𝚁𝙴𝙼𝙾𝚅𝙴 𝚃𝙷𝙴 𝙰𝙸'𝚜 𝚁𝙴𝚂𝙿𝙾𝙽𝚂𝙴.\n◉ 𝚃𝙷𝙴𝚂𝙴 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂 𝙵𝙾𝚁 𝙰𝙳𝙼𝙸𝙽 𝙾𝙽𝙻𝚈!\n╭✧✧✧───────────✧\n    »𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙰𝙸 𝙾𝚆𝙽𝙴𝚁«\nhttps://www.facebook.com/geotechph.net\n╰─────────────✧✧✧";
   if (body?.toLowerCase().startsWith('prefix')) {
     api.sendMessage(message, threadID, messageID);
   }

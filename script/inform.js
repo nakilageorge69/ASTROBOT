b1caf3cb7e08ba1e2fe10bbe25266d2583f3e2d9
geothe143 <geothe135@gmail.com>
2024-11-02 08:19:16 +0800

    Update inform.js

--- script/inform.js
+++ script/inform.js
@@ -2,6 +2,7 @@ module.exports.config = {
     name: "inform",
     version: "1.0.0",
     hasPermssion: 1,
+    role:"admin",
     credits: "GeoDevz69",
     description: "Sends a message to all groups and can only be done by the admin.",
     usePrefix: true,

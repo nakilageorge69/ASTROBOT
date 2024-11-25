72d386d869bc36ab87e026d2dbfa382974edc593
geothe143 <geothe135@gmail.com>
2024-11-25 08:32:48 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 module.exports.config = {
-    name: 'math',
+    name: 'ai',
     version: '1.0.0',
     role: 0,
     hasPrefix: false,
@@ -34,6 +34,7 @@ module.exports.config = {
               'why',
               'when',
               'ask',
+              'arlene',
               'gpt4',
               'is',
               'ano',

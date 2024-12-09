d897c14939bba5f95855d3fe51a1df5557cc1319
geothe143 <geothe135@gmail.com>
2024-12-09 13:33:45 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -77,7 +77,7 @@ module.exports.run = async function({ api, event, args }) {
     });
 
     try {
-        const response = await axios.get('https://haji-mix.onrender.com/v3/hercai', {
+        const response = await axios.get('https://hercai.onrender.com/v3/hercai', {
             params: { question }
         });
         const aiResponse = response.data;

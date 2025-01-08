13a4c51b8e662564fa74d20a62fb35ab6dc58786
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-01-08 18:15:12 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -35,7 +35,7 @@ module.exports.config = {
               'when',
               'ask',
               'arlene',
-              'gpt4',
+              'gpt',
               'is',
               'ano',
               'paano',
@@ -77,7 +77,7 @@ module.exports.run = async function({ api, event, args }) {
     });
 
     try {
-        const response = await axios.get('https://hercai.onrender.com/v3/hercai', {
+        const response = await axios.get('https://kaiz-apis.gleeze.com/v3/hercai', {
             params: { question }
         });
         const aiResponse = response.data;

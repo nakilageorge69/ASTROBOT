2e4aadfd982884435d98d1efaf1c3ebed4c29897
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-01-08 12:23:34 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const login = require('ryuu-fca-api');
+const login = require('chatbox-fca-remake');
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
